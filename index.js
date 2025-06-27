f171045a2f621d4f1ed2cc252e46cd9ba265ff8b
By-num <bynumst3cy@gmail.com>
2025-06-27 15:40:50 +0300

    dom-modifying-element-lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.getElementById('main'); // Selects the <main> element
+main.remove(); // Removes it from the page
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "Bynum is the champion"; // Sets the text inside the <h1>
+
+document.body.appendChild(newHeader); // Adds the <h1> to the page
